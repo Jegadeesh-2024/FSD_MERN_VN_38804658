8684fc887157cca09d3c6f224fb10275c00bd61e
Jegadeesh _G _ dev <jegadeeshvijay75@gmail.com>
2025-11-23 13:16:02 +0530

    Add files via upload

---
+++ Assignments/Assignment-4/SearchBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SearchBar = ({search,setSearch}) => {
+  return (
+    <div>
+        <input
+      type="text"
+      placeholder="Search user by name..."
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+      style={{ padding: 8, width: "300px", marginBottom: 20 }}
+    />
+      
+    </div>
+  )
+}
+
+export default SearchBar
